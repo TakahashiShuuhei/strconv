51706e3de9d529b67dcafb32c73e7e4255dfcafa
uhiaha888 <uhiaha888@gmail.com>
2024-12-07 12:25:50 +0900

    変換処理を追加

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,72 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// コンテキストメニューコマンドを登録
+	let disposable = vscode.commands.registerTextEditorCommand('strconv.decodeString', 
+		async (editor: vscode.TextEditor) => {
+			// 選択されているテキストを取得
+			const selection = editor.selection;
+			const text = editor.document.getText(selection);
+			
+			if (!text) {
+				vscode.window.showWarningMessage('テキストが選択されていません');
+				return;
+			}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "strconv" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('strconv.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from strconv!');
-	});
+			// デコード処理を実行
+			const results = await decodeWithVariousMethods(text);
+			
+			// 新しいタブで結果を表示
+			const document = await vscode.workspace.openTextDocument({
+				content: formatResults(results),
+				language: 'text'
+			});
+			
+			vscode.window.showTextDocument(document);
+		});
 
 	context.subscriptions.push(disposable);
+}
+
+interface DecodingResult {
+	method: string;
+	result: string;
+}
+
+async function decodeWithVariousMethods(text: string): Promise<DecodingResult[]> {
+	const results: DecodingResult[] = [];
+	
+	try {
+		// URLデコード
+		results.push({
+			method: 'URL Decode',
+			result: decodeURIComponent(text)
+		});
+		
+		// Base64デコード
+		results.push({
+			method: 'Base64 Decode',
+			result: Buffer.from(text, 'base64').toString('utf-8')
+		});
+		
+		// Unicode エスケープシーケンスのデコード
+		results.push({
+			method: 'Unicode Escape',
+			result: text.replace(/\\u([0-9a-fA-F]{4})/g, 
+				(_, p1) => String.fromCharCode(parseInt(p1, 16)))
+		});
+		
+	} catch (error) {
+		// エラーが発生しても処理を継続
+	}
+	
+	return results;
+}
+
+function formatResults(results: DecodingResult[]): string {
+	return results.map(r => 
+		`=== ${r.method} ===\n${r.result}\n\n`
+	).join('');
 }
 
 // This method is called when your extension is deactivated
